44c508422d6f2f8b1604774b6962d19cf7db2a2c
khurtalexmagdaong-sudo <khurtalexmagdaong@gmail.com>
2026-05-07 15:24:50 +0800

    Wrap app with AuthProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { AppProvider } from './context/AppContext';
+import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
   return (
-    <AppProvider>
-      <RouterProvider router={router} />
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <RouterProvider router={router} />
+      </AppProvider>
+    </AuthProvider>
   );
 }
